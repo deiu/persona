3319e665a32136bc66a5231f2c07ae804cc1dc37
deiu <andrei@fcns.eu>
2014-05-12 17:54:39 -0400

    First try

---
+++ src/app/app.js
@@ -0,0 +1,25 @@
+angular.module( 'Persona', [
+  'templates-app',
+  'templates-common',
+  'Persona.home',
+  'Persona.about',
+  'ui.router'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'MainCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | Persona' ;
+    }
+  });
+
+})
+
+;
